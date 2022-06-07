acf2651f39c682aac5b816a91000a298bd2e092b
sid-al <sendplusx@gmail.com>
2022-06-07 23:24:14 +0300

    Done.

--- index.js
+++ index.js
@@ -1,1 +1,24 @@
 // Code your solution in this file!
+const returnFirstTwoDrivers = function (cabDrivers) {
+    return cabDrivers.slice(0, 2);
+  };
+  
+  const returnLastTwoDrivers = function (cabDrivers) {
+    return cabDrivers.slice(-2);
+  };
+  
+  const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+  
+  const createFareMultiplier = function (timesValue) {
+    return function (value) {
+      return timesValue * value;
+    };
+  };
+  
+  const fareDoubler = createFareMultiplier(2);
+  
+  const fareTripler = createFareMultiplier(3);
+  
+  const selectDifferentDrivers = function (cabDrivers, driversToReturn) {
+    return driversToReturn(cabDrivers);
+  };
